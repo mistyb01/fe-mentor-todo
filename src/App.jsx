8572ff054628b8edc7614f0668a9db153abd6ecb
Misty Becker <mistyb01@uw.edu>
2022-11-27 04:42:10 -0800

    react setup

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+    Todo
+  
+    {/* <!-- Add dynamic number --> */} x items left
+  
+    All
+    Active 
+    Completed
+  
+    Clear Completed
+  
+    Drag and drop to reorder list
+  
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
+  </>
+  );
+}
+
+export default App;
